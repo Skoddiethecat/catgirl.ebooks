f252d6c62fb10f48cd2d014cf5a8ad69412b6045
Skoddie <Skoddiethecat@users.noreply.github.com>
2018-12-29 19:45:24 -0800

    Create dev branch

--- catgirl.ebooks.py
+++ catgirl.ebooks.py
@@ -1,14 +1,25 @@
-import os
-from markovbot import MarkovBot
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy, sys
 from catgirlebookscreds import *
 
-tweetbot = MarkovBot()
+argfile = str(sys.argv[1])
 
-dirname = os.path.dirname(os.path.abspath(__file__))
-book = os.path.join(dirname, 'storyofheavens.txt')
+auth = tweepy.OAuthHandler(cons_key, cons_secret)
+auth.set_access_toek(access_token, access_token_secret)
 
-tweetbot.read(book)
+openfile = open(argfile, r)
+file = openfile.readlines()
+openfile.close()
 
-tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
 
-tweetbot.twitter_tweeting_start(days=0, hours=8, keywords=None, prefix=None, suffix=None)
+
+# import os
+# from markovbot import MarkovBot
+# tweetbot = MarkovBot()
+# dirname = os.path.dirname(os.path.abspath(__file__))
+# book = os.path.join(dirname, 'storyofheavens.txt')
+# tweetbot.read(book)
+# tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
+# tweetbot.twitter_tweeting_start(days=0, hours=8, keywords=None, prefix=None, suffix=None)
